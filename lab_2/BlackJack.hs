c582a246c9092cd005007ad1ae7c1f633e2ef623
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-11-07 18:48:04 +0100

    Task 3.2 finished

--- lab_2/BlackJack.hs
+++ lab_2/BlackJack.hs
@@ -1,6 +1,21 @@
 
 -- Part A: Task 3.1 (reading the document),
--- Task 3.2 (writing out what happens to the size function)
+-- Task 3.2 Size function description:
+-- The signature of the function describes that from a hand we
+-- obtain a number representing how many cards are stacked in the
+-- Hand.
+-- To calculate the function, the recursive nature of the hand definition is
+-- considered.
+-- An empty hand's size is 0. This is the base case for a hand.
+-- size Empty            = 0
+-- The only other possible case happens if the hand is not empty.
+-- If that is the case, the structure of the hand consists of
+-- a card and a hand that have been 'added'.
+-- When this happens, we know that the size of that structure
+-- is one (card) plus the size of the hand without that card.
+-- This allows to pattern-match a type describing a card in order
+-- to generate a sum of ones representing the size.
+
 -- and part of Task 3.3,
 -- implement the function "empty",
 -- implement the function "value"
