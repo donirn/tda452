f0e95c588ba842fc27ac3c549e572e08d8052d26
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-11-07 18:36:16 +0100

    PM for lab 2A

---
+++ lab_2/BlackJack.hs
@@ -0,0 +1,8 @@
+
+-- Part A: Task 3.1 (reading the document),
+-- Task 3.2 (writing out what happens to the size function)
+-- and part of Task 3.3,
+-- implement the function "empty",
+-- implement the function "value"
+-- implement the function "gameOver"
+-- implement the function "winner".
