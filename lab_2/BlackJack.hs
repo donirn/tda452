267518cec6848969b2a6d4bfa65188f63a08ff87
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-11-07 20:07:06 +0100

    value :: Hand -> Integer has been implemented. Some examples have been added.

--- lab_2/BlackJack.hs
+++ lab_2/BlackJack.hs
@@ -31,8 +31,21 @@ valueCard card = valueRank (rank card)
 
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
-numberOfAces (Add (Card Ace suit) hand) = 1 + numberOfAces hand 
+numberOfAces (Add (Card Ace suit) hand) = 1 + numberOfAces hand
 numberOfAces (Add card hand) = numberOfAces hand
+
+value :: Hand -> Integer
+value Empty = 0
+value (Add card hand) =
+  let tempValue = value hand + valueCard card
+  in if tempValue > 21
+    then tempValue - (numberOfAces (Add card hand) * 10)
+    else tempValue
 
 -- implement the function "gameOver"
 -- implement the function "winner".
+card1 = Card King Spades -- 10
+card2 = Card King Spades -- 11
+card3 = Card (Numeric 7) Spades -- 4
+hand1 = (Add card3 (Add card2 (Add card1 Empty)))
+hand2 = (Add card3 (Add card2 (Add card1 Empty)))
