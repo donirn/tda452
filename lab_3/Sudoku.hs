4156189f453f4daa0bde9a07a3265622226238f4
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-11-21 20:05:01 +0100

    Shared session backup

--- lab_3/Sudoku.hs
+++ lab_3/Sudoku.hs
@@ -1,7 +1,7 @@
 module Sudoku where
 import Data.Maybe
 import Test.QuickCheck
-
+import Data.Char
 -------------------------------------------------------------------------
 
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
@@ -10,12 +10,13 @@ data Sudoku = Sudoku { rows :: [[Maybe Int]] }
 -- allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku {rows = replicate 9 (replicate 9 Nothing)}
+sudokuTest = Sudoku {rows = replicate 9 (replicate 9 (Just 7))}
 
 -- isSudoku sud checks if sud is really a valid representation of a sudoku
 -- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku sud = length (rows sud) == 9 
-                && and (map (\r -> length r == 9 && 
+isSudoku sud = length (rows sud) == 9
+                && and (map (\r -> length r == 9 &&
                                 and (map isSudokuElement r)) (rows sud))
 
 isSudokuElement :: Maybe Int -> Bool
@@ -30,12 +31,26 @@ isSolved sud = and ( map (\r -> and (map (\e -> not (isNothing e)) r )) (rows su
 
 -- printSudoku sud prints a representation of the sudoku sud on the screen
 printSudoku :: Sudoku -> IO ()
-printSudoku = undefined
+printSudoku sud = mapM_ (\r -> putStrLn (map getValue r)) (rows sud)
+
+getValue :: Maybe Int -> Char
+getValue Nothing = '.'
+getValue (Just e) = intToDigit e
 
 -- readSudoku file reads from the file, and either delivers it, or stops
 -- if the file did not contain a sudoku
 readSudoku :: FilePath -> IO Sudoku
-readSudoku = undefined
+readSudoku filePath = do file <- readFile filePath
+                         txtRows <- lines file
+                         return Sudoku {rows = map lineToList txtRows}
+
+lineToList :: String -> [Maybe Int]
+lineToList line = map getIntValue line
+
+getIntValue :: Char ->  Maybe Int
+getIntValue '.'                  = Nothing
+getIntValue  c | c `elem` ['1'..'9'] = Just (digitToInt c)
+               | otherwise           = error "Program error: Not a Sudoku!"
 
 -------------------------------------------------------------------------
 
