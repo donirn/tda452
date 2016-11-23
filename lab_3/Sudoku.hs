0c92129866097c2a221b7d14f45a25c22a87a899
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-11-23 17:56:41 +0100

    Lab3 finished and ready to handout

--- lab_3/Sudoku.hs
+++ lab_3/Sudoku.hs
@@ -6,44 +6,48 @@ import Data.Char
 import System.Random
 import Control.Monad
 import Data.List
---import Data.Matrix
+
 -------------------------------------------------------------------------
+-- Assignment A
 
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
  deriving ( Show, Eq )
 
--- allBlankSudoku is a sudoku with just blanks
+-- Function allBlankSudoku: Returns a sudoku with just blanks.
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku {rows = replicate 9 (replicate 9 Nothing)}
-sudokuTest = Sudoku {rows = replicate 9 (replicate 9 (Just 7))}
 
--- isSudoku sud checks if sud is really a valid representation of a sudoku
--- puzzle
+-- Function isSudoku: Decides if sud is a valid representation
+-- of a sudoku puzzle
 isSudoku :: Sudoku -> Bool
 isSudoku sud = length (rows sud) == 9
                 && and (map (\r -> length r == 9 &&
                                 and (map isSudokuElement r)) (rows sud))
 
+-- Function isSudokuElement: Checks if an element is a valid Sudoku
+-- cell value.
 isSudokuElement :: Maybe Int -> Bool
 isSudokuElement Nothing  = True
 isSudokuElement (Just e) = e `elem` [1..9]
 
--- isSolved sud checks if sud is already solved, i.e. there are no blanks
+-- Function isSolved: Checks if sud is already solved, i.e. there are no blanks
 isSolved :: Sudoku -> Bool
 isSolved sud = and ( map (\r -> and (map (\e -> not (isNothing e)) r )) (rows sud))
 
 -------------------------------------------------------------------------
+-- Assignment B
 
--- printSudoku sud prints a representation of the sudoku sud on the screen
+-- Function printSudoku: Prints a representation of the sudoku sud on the screen
 printSudoku :: Sudoku -> IO ()
-printSudoku sud = mapM_ (\r -> putStrLn (map getValue r)) (rows sud)
+printSudoku sud = mapM_ (\r -> putStrLn (map fromInt r)) (rows sud)
 
-getValue :: Maybe Int -> Char
-getValue Nothing = '.'
-getValue (Just e) = intToDigit e
+-- Function fromInt: Converts a Maybe Int to a char
+fromInt :: Maybe Int -> Char
+fromInt Nothing = '.'
+fromInt (Just e) = intToDigit e
 
--- readSudoku file reads from the file, and either delivers it, or stops
--- if the file did not contain a sudoku
+-- Function readSudoku: Reads a Sudoku from the file, and either delivers it,
+-- or stops if the file did not contain a sudoku
 readSudoku :: FilePath -> IO Sudoku
 readSudoku filePath = do file <- readFile filePath
                          let txtRows = lines file
@@ -51,58 +55,66 @@ readSudoku filePath = do file <- readFile filePath
                          if (isSudoku sud) then return sud
                          else error "Program error: Not a Sudoku!"
 
+-- Function lineToList: Converts a string into a list useful to represent
+-- Sudokus
 lineToList :: String -> [Maybe Int]
-lineToList line = map getIntValue line
+lineToList line = map fromChar line
 
-getIntValue :: Char ->  Maybe Int
-getIntValue '.' = Nothing
-getIntValue  c  = Just (digitToInt c)
+-- Function fromChar: Obtains the int value from a char.
+fromChar :: Char ->  Maybe Int
+fromChar '.' = Nothing
+fromChar  c  = Just (digitToInt c)
 
 
 -------------------------------------------------------------------------
+-- Assigment C
 
--- cell generates an arbitrary cell in a Sudoku
+-- Function cell: returns generator for Sudoku cell
 cell :: Gen (Maybe Int)
 cell = frequency [(1, liftM Just (choose (1,9))), (9, return Nothing)]
 
--- an instance for generating Arbitrary Sudokus
+-- Instance for generating Arbitrary Sudokus
 instance Arbitrary Sudoku where
   arbitrary =
     do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
        return (Sudoku rows)
 
+-- Function prop_Sudoku: Helper to ease automated checking.
 prop_Sudoku :: Sudoku -> Bool
 prop_Sudoku sud = isSudoku sud
 -------------------------------------------------------------------------
--- given a block, checks if that block does not contain the same digit twice.
-
+-- Assignment D
 type Block = [Maybe Int]
 
+-- Function isOkayBlock: given a block, checks if that block does not contain the same digit twice.
 isOkayBlock :: Block -> Bool
 isOkayBlock blk = length (nubBy (\x y -> x /= Nothing &&  x == y) blk) == 9
+                    -- after removing the duplicates the length should be legal
 
-blockCorrectInstance :: Block
-blockCorrectInstance = [Just 1, Just 2, Nothing, Nothing, Just 3, Nothing, Nothing, Nothing, Just 4]
-
-blockIncorrectInstance :: Block
-blockIncorrectInstance = [Just 1, Just 1, Nothing, Nothing, Just 3, Nothing, Nothing, Nothing, Just 4]
-
--------------------------------------------------------------------------
---
-
-
+-- Function Blocks: Extract lists representing the cell regions to be considered
+-- when deciding over the correctness of a solution. (i.e. rows, columns and 3x3
+-- squares)
 blocks :: Sudoku -> [Block]
-blocks sud = rows sud ++ transpose (rows sud) ++ getBlocks (rows sud) 0 []
+blocks sud = rows sud -- Rows
+             ++ transpose (rows sud) -- Columns
+             ++ getBlocks (rows sud) -- 3x3 Squares or 'blocks'
 
--- take rows
-getBlocks :: [[Maybe Int]] -> Int -> [Block] -> [Block]
-getBlocks _ 9 blocks      = blocks
-getBlocks rows iter blocks = getBlocks (dropFirstBlock rows) (iter+1) (takeFirstBlock rows : blocks)
+-- Function getBlocks: Build 3x3 regions from Sudoku's elements.
+getBlocks :: [[Maybe Int]] -> [Block]
+getBlocks [] = []
+getBlocks rows =  takeFirstBlock rows : getBlocks (dropFirstBlock rows)
 
--- take top-left 3x3 block
+-- Function takeFirstBlock: Takes the top-left 3x3 block.
 takeFirstBlock :: [[Maybe Int]] -> Block
 takeFirstBlock rows = take 3 (rows!!0) ++ take 3 (rows!!1) ++ take 3 (rows!!2)
 
--- drop top-left 3x3 block
+-- Function dropFirstBlock: Drops the top-left 3x3 block.
 dropFirstBlock :: [[Maybe Int]] -> [[Maybe Int]]
-dropFirstBlock rows = [drop 3 (rows!!0), drop 3 (rows!!1), drop 3 (rows!!2)] ++ drop 3 rows
+dropFirstBlock rows | length (rows!!0) == 3 = drop 3 rows
+                    | otherwise             = [drop 3 (rows!!0), drop 3 (rows!!1), drop 3 (rows!!2)]
+                                                  ++ drop 3 rows
+
+-- Function isOkay: Checks that all the relevant regions are correct and henceforth
+-- the Sudoku is a valid one.
+isOkay :: Sudoku -> Bool
+isOkay sud = and (map isOkayBlock (blocks sud))
