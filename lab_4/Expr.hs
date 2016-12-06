f558dd1f91160794502d2bff9a2c21cd16d214cc
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2016-12-06 16:14:28 +0100

    Lab 4: Added the Parsing logic

--- lab_4/Expr.hs
+++ lab_4/Expr.hs
@@ -30,13 +30,16 @@ showFactor' :: Expr -> String
 showFactor' (Num n) = showExpr (Num n)
 showFactor' Var     = showExpr Var
 showFactor' e       = "(" ++ showExpr e ++ ")"
-
+---------------------------------------------------------------------------
 -- given an expression, and the value for the variable x
 -- , calculates the value of the expression.
 eval :: Expr -> Double -> Double
 eval (Num n) _   = n
 eval Var x       = x
 eval (Add a b) x = (eval a x) + (eval b x)
-eval (Mul a b) = (eval a x) * (eval b x)
-eval (Cos a) x = cos (eval a x)
-eval (Sin a) x = sin (eval a x)
+eval (Mul a b) x = (eval a x) * (eval b x)
+eval (Cos a) x   = cos (eval a x)
+eval (Sin a) x   = sin (eval a x)
+---------------------------------------------------------------------------
+-- Given a string, tries to interpret the string as an expression, and returns Just of that expression if it succeeds. Otherwise, Nothing will be returned.
+--readExpr :: String -> Maybe Expr
